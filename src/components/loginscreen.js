20c0ed061d83b0f97ed12c99dad7f805066ecee4
56radius <cussmeritmohammed@gmail.com>
2023-10-16 00:59:57 +0100

    login screen done

--- src/components/loginscreen.js
+++ src/components/loginscreen.js
@@ -1,11 +1,40 @@
 import React from "react";
 
-function loginscreen() {
+//importng css file code
+import ".././css/loginscreen.css";
+
+//importing logo
+import logo from ".././images/logo.png";
+
+function LoginScreen() {
   return (
-    <div>
-      <h2> Mtu Security Centre </h2>
+    <div className="login-container">
+      <div className="left">
+        <img
+          src={logo}
+          alt="Logo"
+          style={{
+            width: 500,
+            height: 400,
+          }}
+        />
+      </div>
+      <div className="right">
+        <h2>Mtu Security Centre</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="staffId">Staff ID</label>
+            <input type="text" id="staffId" name="staffId" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default loginscreen;
+export default LoginScreen;
