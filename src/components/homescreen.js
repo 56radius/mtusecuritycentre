7d2366dad1ad6b1a6123afbdc9538315e570da2f
56radius <cussmeritmohammed@gmail.com>
2023-10-16 10:57:58 +0100

    staging files

--- src/components/homescreen.js
+++ src/components/homescreen.js
@@ -3,7 +3,15 @@ import React from "react";
 function homescreen() {
   return (
     <div>
-      <p> Hello hiiiiiiiiiiiiiiiiiiii </p>
+      {/* title and switch */}
+      <div className="title">
+        <h1> Security System Admin Panel </h1>
+      </div>
+
+      {/* Logs, thumbnails for the homescreen */}
+      <div className="logCards">
+        <p> This is my log </p>
+      </div>
     </div>
   );
 }
