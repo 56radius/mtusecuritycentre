2a90d551ad16fe00fdd7708d723f66d392e3c37e
56radius <cussmeritmohammed@gmail.com>
2023-10-18 22:28:58 +0100

    hello

--- src/components/homescreen.js
+++ src/components/homescreen.js
@@ -1,16 +1,30 @@
 import React from "react";
 
+//importing css file code
+import ".././css/homescreen.css";
+
 function homescreen() {
   return (
     <div>
       {/* title and switch */}
-      <div className="title">
+      <div
+        style={{
+          jsutifyContent: "center",
+        }}
+        className="title"
+      >
         <h1> Security System Admin Panel </h1>
+
+        {/* the switch */}
+        <div className="switch">
+          <h2> The switch </h2>
+        </div>
       </div>
 
-      {/* Logs, thumbnails for the homescreen */}
+      {/* Logs, thumbnails for the video */}
       <div className="logCards">
         <p> This is my log </p>
+        <div></div>
       </div>
     </div>
   );
