1d493a0a027021e2d556084b6f481e04ae389090
56radius <cussmeritmohammed@gmail.com>
2023-10-20 22:31:50 +0100

    Creating card

--- src/components/homescreen.js
+++ src/components/homescreen.js
@@ -1,46 +1,23 @@
 import React from "react";
-
-//importing css file code
 import ".././css/homescreen.css";
 
-function homescreen() {
+//importing images
+
+function HomeScreen() {
   return (
     <div>
-      {/* title and switch */}
-      <div
-        style={{
-          jsutifyContent: "center",
-        }}
-        className="title"
-      >
+      <div className="title">
         <h1>Security System Admin Panel</h1>
-
-        {/* the switch */}
         <div className="switch">
-          <h2> System is Armed </h2>
+          <h2>System is Armed</h2>
         </div>
       </div>
 
-      {/* Logs 
-      <div className="logs">
-        <h2> Logs </h2>
-
-        <div>
-          <h2> 300 </h2>
-        </div>
-      </div>
-      */}
-
-      <div className="logCards">
-        {/* first card */}
-        <div className="cards">
-          <h2> hello </h2>
-        </div>
-
-        {/* second card */}
+      <div className="card">
+        <p>This is your card content.</p>
       </div>
     </div>
   );
 }
 
-export default homescreen;
+export default HomeScreen;
