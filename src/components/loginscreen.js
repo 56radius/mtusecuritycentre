3bcb62e4aaae2aa235e9e635b4ddca30710b8b45
56radius <cussmeritmohammed@gmail.com>
2024-02-24 08:55:35 +0100

    Fixing cors policy error

--- src/components/loginscreen.js
+++ src/components/loginscreen.js
@@ -1,14 +1,55 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-import ".././css/loginscreen.css";
+import Swal from "sweetalert2";
 import logo from ".././images/logo.png";
+import ".././css/loginscreen.css";
 
 function LoginScreen() {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/home");
+  const handleLogin = async () => {
+    try {
+      // Simulate a login request
+      const response = await fetch(
+        "https://python-security-system.onrender.com/auth/login",
+        {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: "meritmohammed@gmail.com",
+            password: "123456789",
+          }),
+        }
+      );
+
+      if (response.ok) {
+        // Login successful
+        navigate("/home");
+        Swal.fire({
+          icon: "success",
+          title: "Login Successful",
+          text: "Welcome to Mtu Security Centre!",
+        });
+      } else {
+        // Login failed
+        Swal.fire({
+          icon: "error",
+          title: "Login Failed",
+          text: "Invalid credentials. Please try again.",
+        });
+      }
+    } catch (error) {
+      // Error during login
+      Swal.fire({
+        icon: "error",
+        title: "Login Error",
+        text: "An error occurred during login. Please try again later.",
+      });
+      console.error("Error during login:", error);
+    }
   };
 
   return (
@@ -27,8 +68,8 @@ function LoginScreen() {
         <h2>Mtu Security Centre</h2>
         <form>
           <div className="form-group">
-            <label htmlFor="staffId">Staff ID</label>
-            <input type="text" id="staffId" name="staffId" />
+            <label htmlFor="staffId">Email</label>
+            <input type="text" id="email" name="email" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
